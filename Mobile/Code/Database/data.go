48f125226add4c4d2f27fda4a6c63cf383ac8d18
Neo <joshua01gruenbaum@gmail.com>
2026-09-03 14:55:17 +0200

    Add files via upload

---
+++ Mobile/Code/Database/data.go
@@ -0,0 +1,539 @@
+package MBSEGames
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"strconv"
+	"time"
+
+	"cloud.google.com/go/firestore"
+	firebase "firebase.google.com/go/v4"
+
+	"golang.org/x/oauth2"
+	"google.golang.org/api/option"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+// /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Structures
+// /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+type User struct {
+	DocRef       string
+	UID          string
+	FirstName    string
+	Surname      string
+	Email        string
+	CelNum       string
+	DisplayName  string
+	PicID        string
+	Streak       int
+	TotalScore   int64
+	TotalTime    int64
+	RecordStreak int
+	IsAdmin      bool
+	LastPlayed   time.Time
+}
+
+type Question struct {
+	DocRef      string
+	Subject     string
+	Category    string
+	Outcome     int
+	Level       int
+	Question    string
+	Option1     string
+	Option2     string
+	Option3     string
+	Option4     string
+	Answer      int
+	Explanation string
+}
+
+type Session struct {
+	DocRef           string
+	UserDisplayName  string
+	Subject          string
+	Gamemode         string
+	Ai1              string
+	Ai2              string
+	Ai3              string
+	StartTime        string
+	EndTime          string
+	Score            int64
+	CorrectAnswers   int
+	IncorrectAnswers int
+	QDetails         string
+}
+
+type ChanceCard struct {
+	DocRef        string
+	Message       string
+	IsPosChange   bool
+	IsScoreChange bool
+	IsToFieldMove bool
+	Value         int
+}
+
+type userTokenSource struct {
+	AccessToken string
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Global Variables & Constants
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+var Adjectives = [...]string{"able", "afraid", "angry", "awake", "bad", "beautiful", "big", "bitter", "black", "blue", "bold", "bored", "brave", "bright", "broad", "broken", "brown", "busy", "calm", "certain", "cheap", "clean", "clear", "clever", "close", "cloudy", "cold", "cool", "crazy", "cruel", "dark", "dead", "deep", "difficult", "dirty", "dry", "dull", "early", "easy", "empty", "equal", "excited", "faint", "false", "famous", "far", "fast", "fat", "few", "fine", "firm", "first", "flat", "foreign", "free", "fresh", "full", "funny", "gentle", "glad", "gold", "good", "gray", "great", "green", "happy", "hard", "heavy", "high", "hollow", "hot", "huge", "hungry", "ill", "important", "kind", "large", "last", "late", "lazy", "left", "light", "little", "lively", "lonely", "long", "loud", "low", "mad", "main", "many", "narrow", "near", "neat", "necessary", "new", "next", "nice", "noisy", "normal", "old", "open", "orange", "other", "pale", "past", "pink", "plain", "pleasant", "poor", "polite", "possible", "present", "pretty", "proud", "public", "purple", "quick", "quiet", "rare", "ready", "real", "red", "rich", "right", "rough", "round", "sad", "safe", "salty", "same", "scared", "secret", "serious", "sharp", "short", "shy", "sick", "silent", "silly", "simple", "single", "slow", "small", "smooth", "soft", "solid", "sour", "special", "square", "steady", "steep", "stiff", "strange", "strict", "strong", "stupid", "sudden", "sure", "sweet", "tall", "tame", "tender", "thick", "thin", "thirsty", "tidy", "tight", "tiny", "tired", "true", "ugly", "useful", "usual", "vague", "violet", "warm", "weak", "wet", "white", "whole", "wicked", "wide", "wild", "wise", "wonderful", "wrong", "yellow", "young"}
+var Nouns = [...]string{"air", "animal", "answer", "apple", "area", "arm", "art", "baby", "back", "bag", "ball", "bank", "bed", "bird", "blood", "boat", "body", "bone", "book", "bottom", "box", "boy", "branch", "bread", "brother", "building", "bus", "car", "card", "case", "cat", "cause", "chair", "child", "city", "class", "cloud", "coat", "color", "company", "corner", "country", "course", "cow", "crowd", "cup", "day", "desk", "dog", "door", "dream", "dress", "ear", "earth", "egg", "end", "eye", "face", "fact", "family", "father", "fear", "field", "figure", "finger", "fire", "fish", "floor", "flower", "food", "foot", "force", "forest", "friend", "front", "fruit", "game", "garden", "girl", "glass", "gold", "government", "grass", "ground", "group", "hair", "hand", "hat", "head", "health", "heart", "hill", "home", "horse", "hospital", "hour", "house", "ice", "idea", "image", "industry", "island", "job", "key", "kind", "king", "kitchen", "lake", "land", "language", "law", "leaf", "leg", "letter", "level", "life", "light", "line", "list", "love", "machine", "man", "map", "market", "material", "matter", "meal", "meat", "member", "message", "metal", "milk", "mind", "minute", "money", "month", "moon", "morning", "mother", "mountain", "mouth", "music", "name", "nation", "neck", "needle", "neighbor", "network", "news", "night", "noise", "north", "nose", "note", "number", "ocean", "office", "oil", "order", "page", "paint", "paper", "parent", "park", "part", "party", "past", "path", "pen", "pencil", "people", "person", "phone", "picture", "piece", "pig", "place", "plan", "plane", "plant", "plate", "point", "police", "pool", "power", "president", "price", "problem", "product", "program", "property", "question", "radio", "rain", "reason", "record", "report", "rest", "result", "rice", "river", "road", "rock", "room", "root", "rule", "salt", "sand", "school", "science", "sea", "seat", "second", "seed", "sentence", "service", "shape", "sheep", "ship", "shirt", "shoe", "shop", "show", "side", "sign", "silver", "sister", "size", "skin", "sky", "snow", "soap", "society", "soil", "son", "song", "sound", "soup", "south", "space", "spoon", "sport", "spring", "square", "star", "state", "station", "steam", "steel", "step", "stick", "stone", "stop", "store", "story", "street", "student", "study", "sugar", "summer", "sun", "system", "table", "tail", "tea", "teacher", "team", "television", "test", "thing", "thought", "time", "toe", "top", "town", "toy", "track", "trade", "train", "tree", "trip", "trouble", "truck", "turn", "tv", "uncle", "unit", "valley", "value", "video", "view", "village", "voice", "wall", "war", "water", "way", "week", "weight", "west", "wheel", "wind", "window", "wine", "winter", "wire", "woman", "wood", "word", "work", "world", "yard", "year"}
+
+const (
+	WebApiKey = "AIzaSyBSzixG2InF6Y5dEamzGX0Ka1XQWfnd-uA"
+	ProjectID = "m-b-s-e-moonpoly-v2-0-0-zutmic"
+)
+var CurrentUser User
+
+var Users []User
+var Questions []Question
+var ChanceCards []ChanceCard
+var Sessions []Session
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Intialisation
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+func (t *userTokenSource) Token() (*oauth2.Token, error) {
+	return &oauth2.Token{
+		AccessToken: t.AccessToken,
+		TokenType:   "Bearer",
+		Expiry:      time.Now().Add(10 * time.Hour), 
+	}, nil
+}
+
+func InitializeFirebaseWithUser(ctx context.Context, idToken string, projectID string) (*firebase.App, *firestore.Client, error) {
+	
+    // Create the "Bridge"
+	tokenSource := &userTokenSource{AccessToken: idToken}
+	
+	conf := &firebase.Config{ProjectID: projectID}
+	opt := option.WithTokenSource(tokenSource)
+
+	// Initialize App
+	app, err := firebase.NewApp(ctx, conf, opt)
+	if err != nil {
+		return nil, nil, fmt.Errorf("error initializing app: %w", err)
+	}
+
+	// Initialize Firestore (Authenticated as the User)
+	client, err := app.Firestore(ctx)
+	if err != nil {
+		return nil, nil, fmt.Errorf("error getting Firestore client: %w", err)
+	}
+
+	return app, client, nil
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Users
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Login
+func Login(ctx context.Context, app *firebase.App, firestoreClient *firestore.Client, email string) error {
+	docGrab := firestoreClient.Collection("users").Where("email", "==", email).Documents(ctx)
+	for {
+		doc, err := docGrab.Next()
+		if err != nil {
+			if err.Error() == "no more items in iterator" {
+				break
+			}
+			return err
+		}
+
+		var u User
+		doc.DataTo(&u)
+
+		CurrentUser.DocRef = doc.Ref.ID
+		CurrentUser.UID = doc.Ref.ID
+		CurrentUser.FirstName = u.FirstName
+		CurrentUser.Surname = u.Surname
+		CurrentUser.Email = email
+		CurrentUser.CelNum = u.CelNum
+		CurrentUser.DisplayName = u.DisplayName
+		CurrentUser.PicID = u.PicID
+		CurrentUser.Streak = u.Streak
+		CurrentUser.TotalScore = u.TotalScore
+		CurrentUser.TotalTime = u.TotalTime
+		CurrentUser.RecordStreak = u.RecordStreak
+		CurrentUser.IsAdmin = u.IsAdmin
+		CurrentUser.LastPlayed = u.LastPlayed
+	}
+
+	err := GetQuestions(ctx, firestoreClient)
+	if err != nil {
+		return err
+	}
+
+	err = GetChanceCards(ctx, firestoreClient)
+	if err != nil {
+		return err
+	}
+
+	err = GetUsers(ctx, firestoreClient)
+	if err != nil {
+		return err
+	}
+
+	return nil
+
+}
+
+// List
+func GetUsers(ctx context.Context, firestoreClient *firestore.Client) error {
+	i := 0
+	iter := firestoreClient.Collection("users").Documents(ctx)
+	for {
+		doc, err := iter.Next()
+		if err != nil {
+			if err.Error() == "no more items in iterator" {
+				break
+			}
+			return err
+		}
+
+		var user, u User
+		doc.DataTo(&u)
+		user.DocRef = doc.Ref.ID
+		user.UID = u.UID
+		user.FirstName = u.FirstName
+		user.Surname = u.Surname
+		user.Email = u.Email
+		user.CelNum = u.CelNum
+		user.DisplayName = u.DisplayName
+		user.PicID = u.PicID
+		user.Streak = u.Streak
+		user.TotalScore = u.TotalScore
+		user.TotalTime = u.TotalTime
+		user.RecordStreak = u.RecordStreak
+		user.IsAdmin = u.IsAdmin
+		user.LastPlayed = u.LastPlayed
+
+		if len(Users) > i {
+			Users[i] = user
+		} else {
+			Users = append(Users, user)
+		}
+
+		i++
+	}
+
+	return nil
+}
+
+// Edit User
+func EditUser(ctx context.Context, client *firestore.Client, DocRef, Field, NewstrValue string, NewintValue int, Newint64Value int64) (error, string) {
+	for i := range Users {
+		if Users[i].DocRef == DocRef {
+			switch Field {
+
+			case "FirstName":
+				Users[i].FirstName = NewstrValue
+				//Create an array of updates
+				updates := []firestore.Update{
+					{Path: "firstname", Value: NewstrValue},
+				}
+
+				//Apply the updates
+				_, err := client.Collection("users").Doc(DocRef).Update(ctx, updates)
+				if err != nil {
+					if status.Code(err) == codes.NotFound {
+						return fmt.Errorf("user %s not found", DocRef), ""
+					}
+					return fmt.Errorf("error updating user: %v", err), ""
+				}
+
+			case "Surname":
+				Users[i].Surname = NewstrValue
+				//Create an array of updates
+				updates := []firestore.Update{
+					{Path: "surname", Value: NewstrValue},
+				}
+
+				//Apply the updates
+				_, err := client.Collection("users").Doc(DocRef).Update(ctx, updates)
+				if err != nil {
+					if status.Code(err) == codes.NotFound {
+						return fmt.Errorf("user %s not found", DocRef), ""
+					}
+					return fmt.Errorf("error updating user: %v", err), ""
+				}
+
+			case "CelNum":
+				Users[i].CelNum = NewstrValue
+				//Create an array of updates
+				updates := []firestore.Update{
+					{Path: "celnum", Value: NewstrValue},
+				}
+
+				//Apply the updates
+				_, err := client.Collection("users").Doc(DocRef).Update(ctx, updates)
+				if err != nil {
+					if status.Code(err) == codes.NotFound {
+						return fmt.Errorf("user %s not found", DocRef), ""
+					}
+					return fmt.Errorf("error updating user: %v", err), ""
+				}
+
+			case "DisplayName":
+				if NewstrValue == "generate" {
+					r := rand.New(rand.NewSource(time.Now().UnixNano()))
+					displayname := Adjectives[r.Intn(len(Adjectives))] + Nouns[r.Intn(len(Nouns))] + strconv.Itoa(r.Intn(10)) + strconv.Itoa(r.Intn(10)) + strconv.Itoa(r.Intn(10)) + strconv.Itoa(r.Intn(10))
+					Users[i].DisplayName = displayname
+					//Create an array of updates
+					updates := []firestore.Update{
+						{Path: "displayname", Value: displayname},
+					}
+
+					//Apply the updates
+					_, err := client.Collection("users").Doc(DocRef).Update(ctx, updates)
+					if err != nil {
+						if status.Code(err) == codes.NotFound {
+							return fmt.Errorf("user %s not found", DocRef), ""
+						}
+						return fmt.Errorf("error updating user: %v", err), ""
+					}
+
+					return nil, displayname
+				} else {
+					Users[i].DisplayName = NewstrValue
+					//Create an array of updates
+					updates := []firestore.Update{
+						{Path: "displayname", Value: NewstrValue},
+					}
+
+					//Apply the updates
+					_, err := client.Collection("users").Doc(DocRef).Update(ctx, updates)
+					if err != nil {
+						if status.Code(err) == codes.NotFound {
+							return fmt.Errorf("user %s not found", DocRef), ""
+						}
+						return fmt.Errorf("error updating user: %v", err), ""
+					}
+				}
+
+			case "PicID":
+				Users[i].PicID = NewstrValue
+				//Create an array of updates
+				updates := []firestore.Update{
+					{Path: "picid", Value: NewstrValue},
+				}
+
+				//Apply the updates
+				_, err := client.Collection("users").Doc(DocRef).Update(ctx, updates)
+				if err != nil {
+					if status.Code(err) == codes.NotFound {
+						return fmt.Errorf("user %s not found", DocRef), ""
+					}
+					return fmt.Errorf("error updating user: %v", err), ""
+				}
+
+			case "Streak":
+				Users[i].Streak = NewintValue
+				//Create an array of updates
+				updates := []firestore.Update{
+					{Path: "streak", Value: NewintValue},
+				}
+
+				//Apply the updates
+				_, err := client.Collection("users").Doc(DocRef).Update(ctx, updates)
+				if err != nil {
+					if status.Code(err) == codes.NotFound {
+						return fmt.Errorf("user %s not found", DocRef), ""
+					}
+					return fmt.Errorf("error updating user: %v", err), ""
+				}
+
+			case "TotalScore":
+				Users[i].TotalScore = Newint64Value
+				//Create an array of updates
+				updates := []firestore.Update{
+					{Path: "totalscore", Value: Newint64Value},
+				}
+
+				//Apply the updates
+				_, err := client.Collection("users").Doc(DocRef).Update(ctx, updates)
+				if err != nil {
+					if status.Code(err) == codes.NotFound {
+						return fmt.Errorf("user %s not found", DocRef), ""
+					}
+					return fmt.Errorf("error updating user: %v", err), ""
+				}
+
+			case "TotalTime":
+				Users[i].TotalTime = Newint64Value
+				//Create an array of updates
+				updates := []firestore.Update{
+					{Path: "totaltime", Value: Newint64Value},
+				}
+
+				//Apply the updates
+				_, err := client.Collection("users").Doc(DocRef).Update(ctx, updates)
+				if err != nil {
+					if status.Code(err) == codes.NotFound {
+						return fmt.Errorf("user %s not found", DocRef), ""
+					}
+					return fmt.Errorf("error updating user: %v", err), ""
+				}
+
+			case "RecordStreak":
+				Users[i].RecordStreak = NewintValue
+				//Create an array of updates
+				updates := []firestore.Update{
+					{Path: "recordstreak", Value: NewintValue},
+				}
+
+				//Apply the updates
+				_, err := client.Collection("users").Doc(DocRef).Update(ctx, updates)
+				if err != nil {
+					if status.Code(err) == codes.NotFound {
+						return fmt.Errorf("user %s not found", DocRef), ""
+					}
+					return fmt.Errorf("error updating user: %v", err), ""
+				}
+
+			case "LastPlayed":
+				tnow := time.Now()
+				Users[i].LastPlayed = tnow
+
+				//Create an array of updates
+				updates := []firestore.Update{
+					{Path: "lastplayed", Value: tnow},
+				}
+
+				//Apply the updates
+				_, err := client.Collection("users").Doc(DocRef).Update(ctx, updates)
+				if err != nil {
+					if status.Code(err) == codes.NotFound {
+						return fmt.Errorf("user %s not found", DocRef), ""
+					}
+					return fmt.Errorf("error updating user: %v", err), ""
+				}
+			}
+
+			break
+		}
+	}
+	return nil, ""
+}
+
+// /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Questions
+// /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// List
+func GetQuestions(ctx context.Context, firestoreClient *firestore.Client) error {
+	i := 0
+	iter := firestoreClient.Collection("questions").Documents(ctx)
+	for {
+		doc, err := iter.Next()
+		if err != nil {
+			if err.Error() == "no more items in iterator" {
+				break
+			}
+			return err
+		}
+
+		qRef := doc.Ref.ID
+		var q Question
+		doc.DataTo(&q)
+		q.DocRef = qRef
+
+		if len(Questions) > i {
+			Questions[i] = q
+		} else {
+			Questions = append(Questions, q)
+		}
+
+		i++
+
+	}
+
+	return nil
+}
+
+// /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Chance Cards
+// /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// List
+func GetChanceCards(ctx context.Context, firestoreClient *firestore.Client) error {
+	i := 0
+	iter := firestoreClient.Collection("tac").Documents(ctx)
+	for {
+		doc, err := iter.Next()
+		if err != nil {
+			if err.Error() == "no more items in iterator" {
+				break
+			}
+			return err
+		}
+
+		var chancecard, c ChanceCard
+		doc.DataTo(&c)
+		chancecard.DocRef = doc.Ref.ID
+		chancecard.Message = c.Message
+		chancecard.IsPosChange = c.IsPosChange
+		chancecard.IsScoreChange = c.IsScoreChange
+		chancecard.IsToFieldMove = c.IsToFieldMove
+		chancecard.Value = c.Value
+
+		if len(ChanceCards) > i {
+			ChanceCards[i] = chancecard
+		} else {
+			ChanceCards = append(ChanceCards, chancecard)
+		}
+
+		i++
+	}
+
+	return nil
+}
+// /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Sessions
+// /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Create
+func AddSession(ctx context.Context, firestoreClient *firestore.Client, UserDisplayName, Subject, Gamemode, Ai1, Ai2, Ai3, StartTime, EndTime, QDetails string, Score int64, CorrectAnswers, IncorrectAnswers int) error {
+	data := map[string]interface{}{
+		"userdisplayname":  UserDisplayName,
+		"subject":          Subject,
+		"gamemode":         Gamemode,
+		"ai1":              Ai1,
+		"ai2":              Ai2,
+		"ai3":              Ai3,
+		"starttime":        StartTime,
+		"endtime":          EndTime,
+		"score":            Score,
+		"numcorrect":       CorrectAnswers,
+		"numincorrect":     IncorrectAnswers,
+		"questionsdetails": QDetails,
+	}
+
+	docRef, _, err := firestoreClient.Collection("sessions").Add(ctx, data)
+	if err != nil {
+		return err
+	}
+
+	var session Session
+	session.DocRef = docRef.ID
+	session.UserDisplayName = UserDisplayName
+	session.Subject = Subject
+	session.Gamemode = Gamemode
+	session.Ai1 = Ai1
+	session.Ai2 = Ai2
+	session.Ai3 = Ai3
+	session.StartTime = StartTime
+	session.EndTime = EndTime
+	session.Score = Score
+	session.CorrectAnswers = CorrectAnswers
+	session.IncorrectAnswers = IncorrectAnswers
+	session.QDetails = QDetails
+
+	Sessions = append(Sessions, session)
+
+	return nil
+}
---
+++ Mobile/go.mod
@@ -0,0 +1,89 @@
+module MBSEGames
+
+go 1.24.1
+
+require (
+	cloud.google.com/go/firestore v1.20.0
+	cogentcore.org/core v0.3.12
+	firebase.google.com/go/v4 v4.18.0
+	golang.org/x/oauth2 v0.33.0
+	google.golang.org/api v0.257.0
+	google.golang.org/grpc v1.77.0
+)
+
+require (
+	cel.dev/expr v0.24.0 // indirect
+	cloud.google.com/go v0.121.6 // indirect
+	cloud.google.com/go/auth v0.17.0 // indirect
+	cloud.google.com/go/auth/oauth2adapt v0.2.8 // indirect
+	cloud.google.com/go/compute/metadata v0.9.0 // indirect
+	cloud.google.com/go/iam v1.5.2 // indirect
+	cloud.google.com/go/longrunning v0.6.7 // indirect
+	cloud.google.com/go/monitoring v1.24.2 // indirect
+	cloud.google.com/go/storage v1.56.0 // indirect
+	github.com/Bios-Marcel/wastebasket/v2 v2.0.3 // indirect
+	github.com/GoogleCloudPlatform/opentelemetry-operations-go/detectors/gcp v1.30.0 // indirect
+	github.com/GoogleCloudPlatform/opentelemetry-operations-go/exporter/metric v0.53.0 // indirect
+	github.com/GoogleCloudPlatform/opentelemetry-operations-go/internal/resourcemapping v0.53.0 // indirect
+	github.com/Masterminds/vcs v1.13.3 // indirect
+	github.com/MicahParks/keyfunc v1.9.0 // indirect
+	github.com/alecthomas/chroma/v2 v2.13.0 // indirect
+	github.com/anthonynsimon/bild v0.13.0 // indirect
+	github.com/aymerick/douceur v0.2.0 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/chewxy/math32 v1.10.1 // indirect
+	github.com/cncf/xds/go v0.0.0-20251022180443-0feb69152e9f // indirect
+	github.com/cogentcore/webgpu v0.23.0 // indirect
+	github.com/dlclark/regexp2 v1.11.0 // indirect
+	github.com/envoyproxy/go-control-plane/envoy v1.35.0 // indirect
+	github.com/envoyproxy/protoc-gen-validate v1.2.1 // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/fsnotify/fsnotify v1.8.0 // indirect
+	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20240506104042-037f3cc74f2a // indirect
+	github.com/go-jose/go-jose/v4 v4.1.3 // indirect
+	github.com/go-logr/logr v1.4.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/go-text/typesetting v0.3.1-0.20250402122313-7a0f05577ff5 // indirect
+	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/golang-jwt/jwt/v4 v4.5.2 // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/google/s2a-go v0.1.9 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.3.7 // indirect
+	github.com/googleapis/gax-go/v2 v2.15.0 // indirect
+	github.com/gorilla/css v1.0.1 // indirect
+	github.com/h2non/filetype v1.1.3 // indirect
+	github.com/hack-pad/go-indexeddb v0.3.2 // indirect
+	github.com/hack-pad/hackpadfs v0.2.1 // indirect
+	github.com/hack-pad/safejs v0.1.1 // indirect
+	github.com/jinzhu/copier v0.4.0 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/pelletier/go-toml/v2 v2.1.2-0.20240227203013-2b69615b5d55 // indirect
+	github.com/planetscale/vtprotobuf v0.6.1-0.20240319094008-0393e58bdf10 // indirect
+	github.com/spiffe/go-spiffe/v2 v2.6.0 // indirect
+	github.com/tdewolff/parse/v2 v2.7.19 // indirect
+	go.opentelemetry.io/auto/sdk v1.2.1 // indirect
+	go.opentelemetry.io/contrib/detectors/gcp v1.38.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.61.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.61.0 // indirect
+	go.opentelemetry.io/otel v1.38.0 // indirect
+	go.opentelemetry.io/otel/metric v1.38.0 // indirect
+	go.opentelemetry.io/otel/sdk v1.38.0 // indirect
+	go.opentelemetry.io/otel/sdk/metric v1.38.0 // indirect
+	go.opentelemetry.io/otel/trace v1.38.0 // indirect
+	golang.org/x/crypto v0.45.0 // indirect
+	golang.org/x/exp v0.0.0-20231110203233-9a3e6036ecaa // indirect
+	golang.org/x/image v0.25.0 // indirect
+	golang.org/x/mod v0.29.0 // indirect
+	golang.org/x/net v0.47.0 // indirect
+	golang.org/x/sync v0.18.0 // indirect
+	golang.org/x/sys v0.38.0 // indirect
+	golang.org/x/text v0.31.0 // indirect
+	golang.org/x/time v0.14.0 // indirect
+	golang.org/x/tools v0.38.0 // indirect
+	google.golang.org/appengine/v2 v2.0.6 // indirect
+	google.golang.org/genproto v0.0.0-20250603155806-513f23925822 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20251022142026-3a174f9686a8 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20251124214823-79d6a2a48846 // indirect
+	google.golang.org/protobuf v1.36.10 // indirect
+)
